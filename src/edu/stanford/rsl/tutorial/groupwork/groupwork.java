236fb1c9ae3f2ace459ad72e7ffff58b3ceb3f85
Philipp Klumpp <ac62yqoc@stud.informatik.uni-erlangen.de>
2015-10-20 12:23:28 +0200

    final result of EX1 a-c

--- src/edu/stanford/rsl/tutorial/groupwork/groupwork.java
+++ src/edu/stanford/rsl/tutorial/groupwork/groupwork.java
@@ -1,9 +1,12 @@
 package edu.stanford.rsl.tutorial.groupwork;
 
+import ij.ImageJ;
+
 public class groupwork {
 	
 	public static void main(String args[]) {
 		CustomPhantom phantom1 = new CustomPhantom(400, 400);
+		new ImageJ();
 		phantom1.show();
 	}
 }
