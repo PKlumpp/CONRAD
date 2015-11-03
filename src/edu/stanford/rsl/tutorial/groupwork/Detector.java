120f8c09aab7ebe9f542c83efe71de023f905f63
Philipp Klumpp <ac62yqoc@stud.informatik.uni-erlangen.de>
2015-11-03 11:44:52 +0100

    this is final version of exercise 1

--- src/edu/stanford/rsl/tutorial/groupwork/Detector.java
+++ src/edu/stanford/rsl/tutorial/groupwork/Detector.java
@@ -1,9 +1,5 @@
 package edu.stanford.rsl.tutorial.groupwork;
 
-import com.sun.scenario.effect.impl.prism.PrReflectionPeer;
-
-import weka.filters.unsupervised.attribute.Center;
-import edu.stanford.rsl.conrad.data.Grid;
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
 import edu.stanford.rsl.conrad.data.numeric.InterpolationOperators;
 
@@ -43,7 +39,6 @@ public class Detector {
 	}
 
 	public Grid2D getSinogram(CustomPhantom phantom) {
-		long time = System.currentTimeMillis() + 10;
 		Grid2D sinogram = new Grid2D(projections, pixels);
 		sinogram.setOrigin(0, (-pixels / 2 + 0.5) * spacing);
 		sinogram.setSpacing(180.0f / projections, spacing);
@@ -51,21 +46,21 @@ public class Detector {
 			float angle = 180f * projection / projections;
 			double gradient = Math.sin(Math.toRadians(angle))
 					/ Math.cos(Math.toRadians(angle));
+			double x_step = Math.abs(Math.sin(Math.toRadians(270 - angle))
+					* phantom.getSpacing()[0]);
+			double y_step = Math.sin(Math.toRadians(angle + 180))
+					* -Math.signum(gradient) * phantom.getSpacing()[1];
 			for (double pixel = sinogram.getOrigin()[1]; pixel <= sinogram
-					.getOrigin()[1] + pixels * spacing; pixel+=spacing) {
+					.getOrigin()[1] + pixels * spacing; pixel += spacing) {
 				double[] pixel_pos = { Math.sin(Math.toRadians(angle)) * pixel,
 						Math.cos(Math.toRadians(angle)) * pixel * -1 };
-				if (projection == 250 && pixel == 0.5) {
-					int x = 2;
-					x++;
-				}
 				double[][] intersects = new LineInBox(phantom.indexToPhysical(
 						phantom.getWidth() - 1, phantom.getHeight() - 1)[0],
-						phantom.indexToPhysical(0, 0)[1],
-						phantom.indexToPhysical(0, 0)[0],
 						phantom.indexToPhysical(phantom.getWidth() - 1,
-								phantom.getHeight() - 1)[1], gradient,
-						pixel_pos).getBoxIntersects();
+								phantom.getHeight() - 1)[1],
+						phantom.indexToPhysical(0, 0)[0],
+						phantom.indexToPhysical(0, 0)[1], gradient, pixel_pos)
+						.getBoxIntersects();
 				if (intersects[0][0] == -1 && intersects[0][1] == -1
 						&& intersects[1][0] == -1 && intersects[1][1] == -1) {
 					sinogram.setAtIndex(
@@ -73,13 +68,12 @@ public class Detector {
 							(int) sinogram.physicalToIndex(angle, pixel)[1], 0);
 					continue;
 				}
-				double x_step = Math.abs(Math.sin(Math.toRadians(270 - angle))
-						* phantom.getSpacing()[0]);
-				double y_step = Math.sin(Math.toRadians(angle+180)) * -Math.signum(gradient);
-				
+
 				int steps = (int) Math.floor(Math.sqrt(Math.pow(
-						intersects[0][0] - intersects[1][0], 2)
-						+ Math.pow(intersects[0][1] - intersects[1][1], 2)));
+						(intersects[0][0] - intersects[1][0])
+								/ phantom.getSpacing()[0], 2)
+						+ Math.pow((intersects[0][1] - intersects[1][1])
+								/ phantom.getSpacing()[1], 2)));
 				float value = 0.0f;
 				for (int element = 0; element < steps; element++) {
 					double x_real = intersects[0][0] + element * x_step;
@@ -89,18 +83,9 @@ public class Detector {
 							phantom.physicalToIndex(x_real, y_real)[1]);
 				}
 				value = value / steps;
-				if (projection > 20 && value == 0) {
-					int x = 2;
-					x++;
-				}
-				sinogram.setAtIndex(
-						(int) Math.round(sinogram.physicalToIndex(angle, pixel)[0]),
-						(int) Math.round(sinogram.physicalToIndex(angle, pixel)[1]), value);
-				int test2 = (int) Math.round(sinogram.physicalToIndex(angle, pixel)[0]);
-				int test3 = (int) Math.round(sinogram.physicalToIndex(angle, pixel)[1]);
-				if (projection==251) {
-					int z = 0;
-				}
+				sinogram.setAtIndex((int) Math.round(sinogram.physicalToIndex(
+						angle, pixel)[0]), (int) Math.round(sinogram
+						.physicalToIndex(angle, pixel)[1]), value);
 			}
 		}
 		return sinogram;
--- src/edu/stanford/rsl/tutorial/groupwork/groupwork.java
+++ src/edu/stanford/rsl/tutorial/groupwork/groupwork.java
@@ -5,13 +5,13 @@ import ij.ImageJ;
 public class groupwork {
 	
 	public static void main(String args[]) {
-		CustomPhantom phantom1 = new CustomPhantom(400, 400);
-		phantom1.setOrigin(-phantom1.getWidth()/2+0.5,phantom1.getHeight()/2-0.5);
-		phantom1.setSpacing(1.0f, -1.0f);
-		phantom1.show();
-		Detector detector = new Detector(500, 500, 1);
-		detector.getSinogram(phantom1).show();
 		new ImageJ();
+		CustomPhantom phantom1 = new CustomPhantom(400, 400);
+		phantom1.setSpacing(2.0f, 2.0f);
+		phantom1.setOrigin(-phantom1.getSpacing()[0]*phantom1.getWidth()/2+0.5,-phantom1.getSpacing()[1]*phantom1.getHeight()/2+0.5);
+		phantom1.show();
+		Detector detector = new Detector(500, 250, 2);
+		detector.getSinogram(phantom1).show();
 		//phantom1.show();
 	}
 }
